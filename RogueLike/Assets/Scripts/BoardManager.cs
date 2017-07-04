39862844d41f6b50bfe4fdff4515aec95c4dca72
Jonathan <jonathantruest@gmail.com>
2017-07-04 22:43:22 +0100

    Tablero Acabado

--- RogueLike/Assets/Scripts/BoardManager.cs
+++ RogueLike/Assets/Scripts/BoardManager.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoardManager : MonoBehaviour {
+public class BoardManager : MonoBehaviour
+{
 
     public int colums = 8;
     public int rows = 8;
@@ -10,8 +11,67 @@ public class BoardManager : MonoBehaviour {
     public GameObject[] floorTiles;
     public GameObject[] outerWallTiles;
 
-    public void SetupScene()
-    {
-        Debug.Log("Ejecutado!");
+    public GameObject[] wallTiles;
+    public GameObject[] foodTiles;
+    public GameObject[] enemyTiles;
+
+    public GameObject exit;
+
+    private List<Vector2> gridPositions = new List<Vector2>();
+
+    private Transform boardHolder;
+
+    private void InitializeList() {
+        gridPositions.Clear();
+        for(int x = 1; x < colums -1; x++) {
+            for(int y = 1; y < rows -1; y++) {
+                gridPositions.Add(new Vector2(x, y));
+            }
+        }
+    }
+
+    private Vector2 RandomPosition() {
+        int randomIndex = Random.Range(0, gridPositions.Count);
+        Vector2 randomPosition = gridPositions[randomIndex];
+        gridPositions.RemoveAt(randomIndex);
+        return randomPosition;
+    }
+
+    private void LayoutObjectAtRandom(GameObject[] tileArray, int min, int max) {
+        int objectCount = Random.Range(min, max + 1);
+        for(int i = 0; i < objectCount; i++) {
+            Vector2 randomPosition = RandomPosition();
+            GameObject tileChoice = GetRandomInArray(tileArray);
+            Instantiate(tileChoice, randomPosition, Quaternion.identity);
+        }
+    }
+
+    public void SetupScene() {
+        BoardSetup();
+        InitializeList();
+        LayoutObjectAtRandom(wallTiles, 5, 9);
+        LayoutObjectAtRandom(foodTiles, 1, 5);
+    }
+
+    private void BoardSetup() {
+
+        boardHolder = new GameObject("Board").transform;
+
+        for(int x = -1; x < colums + 1; x++) {
+            for(int y = -1; y < rows +1; y++) {
+                GameObject toInstanciate = GetRandomInArray(floorTiles);
+
+                if (x == -1 || y == -1 || x == colums || y == rows) {
+                    toInstanciate = GetRandomInArray(outerWallTiles);
+                }
+
+                GameObject instance = Instantiate(toInstanciate, new Vector2(x, y), Quaternion.identity);
+                instance.transform.SetParent(boardHolder);
+            }
+        }
+    }
+
+    private GameObject GetRandomInArray(GameObject[] array) {
+        return array[Random.Range(0, array.Length)];
     }
 }

abb4fa431ef1350021ede1f3eaada4470feb9ab3
Jonathan <jonathantruest@gmail.com>
2017-07-04 19:24:00 +0100

    Primera sesion de trabajo

---
+++ RogueLike/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+
+    public int colums = 8;
+    public int rows = 8;
+
+    public GameObject[] floorTiles;
+    public GameObject[] outerWallTiles;
+
+    public void SetupScene()
+    {
+        Debug.Log("Ejecutado!");
+    }
+}
